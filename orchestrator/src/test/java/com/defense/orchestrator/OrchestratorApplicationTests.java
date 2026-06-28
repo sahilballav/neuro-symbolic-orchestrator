e775b06dc1ec127b2e7c985f37a6db31f212bd12
Sahil Ballav <sahilballav@gmail.com>
2026-06-29 02:49:28 +0530

    Phase 3 complete: Added Java Spring Boot Orchestrator and Kafka routing

---
+++ orchestrator/src/test/java/com/defense/orchestrator/OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defense.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
